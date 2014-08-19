a32f877eaf6293cb397dbb32509fd970ca098ccc
Andy Wells <andyandywells@me.com>
2014-08-19 15:07:07 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -18,7 +18,7 @@ while touch() == 'wall':
     turn(1)
     while touch() != 'wall':
       move()
-  elif right_side == 'wall':
+  elif touch() == 'wall':
      turn(1)
      while touch() != 'wall':
       move()
