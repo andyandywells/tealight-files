7dcfc78b0629396478a9f7bb54d2dfdbae60a3bf
Andy Wells <andyandywells@me.com>
2014-08-19 12:01:26 +0100

    Running logo/chess.py

--- logo/chess.py
+++ logo/chess.py
@@ -2,12 +2,9 @@ from tealight.logo import move, turn, color
 
 colors = ["black", "red"]
 
-
-for i in range(0,100):
-  move(i)
-  turn(90)
-  color(colors[i%1])
-  
+turn(45)
+move(100)
+turn(-90)
 for i in range(0,100):
   move(i)
   turn(90)
