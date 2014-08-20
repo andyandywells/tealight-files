0ba2e545b1d28e2bb9653b2c78e063f758fd7156
Andy Wells <andyandywells@me.com>
2014-08-20 12:28:07 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,3 +1,9 @@
 
-Matrix = [[0 for x in xrange(8)] for x in xrange(8)] 
-print(Matrix[0][1])
+Matrix = [[1,2],[3,4]]
+
+for i in range(0,1):
+  for j in range(0,1):
+    if Matrix[i][j] == 3:
+      print("yes")
+  
+  
