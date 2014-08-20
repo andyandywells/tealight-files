2dd77d900c6fc827fafcc70d3dad536408e5cdbf
Andy Wells <andyandywells@me.com>
2014-08-20 12:29:51 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -3,7 +3,7 @@ Matrix = [["1","2"],["3","4"]]
 
 for i in range(0,1):
   for j in range(0,1):
-    if Matrix[i][j] == "3":
+    if Matrix[j] == "3":
       print("yes")
     else:
       print("no")
