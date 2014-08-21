fca56aa7fa1d4c35b2a0f0571978574dfe6945e4
Andy Wells <andyandywells@me.com>
2014-08-21 11:04:31 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,10 +1,12 @@
 global matrix
 def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
-  matrix[0][0] = 1
-  matrix[1][1] = 1
-  matrix[2][2] = 1
-  matrix[3][3] = 1
+  
+  matrix[0][3] = 1
+  matrix[1][2] = 1
+  matrix[3][1] = 1
+  matrix[4][0] = 1
+  
   print(matrix)
   print(len(matrix))
   return matrix
@@ -17,6 +19,8 @@ def checkDiagonalLR():
     for row in range (0,6):
       if matrix[col][row] == 1 and matrix[col][row] == matrix[col+1][row+1] and matrix[col][row] == matrix[col+2][row+2] and matrix[col][row] == matrix[col+3][row+3]:
         print("four in a row")
+      else:
+        print("ttt")
 
 #check diagonal from right to left
 def checkDiagonalRL():
@@ -26,3 +30,4 @@ def checkDiagonalRL():
         print("four in a row")
 
 checkDiagonalLR()
+checkDiagonalRL()
