a7196281d16156e00dd21f4409e926468f1abbea
Andy Wells <andyandywells@me.com>
2014-08-21 10:59:45 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -3,6 +3,8 @@ def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
   matrix[0][0] = 1
   matrix[1][1] = 1
+  matrix[2][2] = 1
+  matrix[3][3] = 1
   print(matrix)
   print(len(matrix))
   return matrix
