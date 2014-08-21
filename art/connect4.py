641d22cb4e2a9f3d67c2124ea36a08b1a45b1aca
Andy Wells <andyandywells@me.com>
2014-08-21 11:23:18 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -29,5 +29,11 @@ def checkDiagonalRL():
       if matrix[row][col] == 1 and matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3]:
         print("four in a row")
 
+#check horizontal left to right
+def checkLR():
+  for row in range(0,6):
+    for col in range (0,6):
+      if matrix[row][col] == 1 and matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3]:
+        print("four in a row")
 
 checkDiagonalRL()
