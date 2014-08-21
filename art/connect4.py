d8499d50ac6c5121ba8731f117f5778b71891fff
Andy Wells <andyandywells@me.com>
2014-08-21 11:02:03 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -11,9 +11,17 @@ def initialiseMatrix():
 
 
 matrix = initialiseMatrix()
+#check diagonal from left to right
+def checkDiagonalLR():
+  for col in range(0,6):
+    for row in range (0,6):
+      if matrix[col][row] == 1 and matrix[col][row] == matrix[col+1][row+1] and matrix[col][row] == matrix[col+2][row+2] and matrix[col][row] == matrix[col+3][row+3]:
+        print("four in a row")
 
-for col in range(0,6):
-  for row in range (0,6):
-    if matrix[col][row] == 1 and matrix[col][row] == matrix[col+1][row+1] and matrix[col][row] == matrix[col+2][row+2] and matrix[col][row] == matrix[col+3][row+3]:
-      print("four in a row")
-  
+#check diagonal from right to left
+def checkDiagonalRL():
+  for col in range(0,6):
+    for row in range (0,6):
+      if matrix[col][row] == 1 and matrix[col][row] == matrix[col-1][row-1] and matrix[col][row] == matrix[col-2][row-2] and matrix[col][row] == matrix[col-3][row-3]:
+        print("four in a row")
+
