5ccb34a9f07fdbb7b23ec98892cf0ca23e356252
Andy Wells <andyandywells@me.com>
2014-08-21 11:16:56 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -24,8 +24,8 @@ def checkDiagonalLR():
 
 #check diagonal from right to left
 def checkDiagonalRL():
-  for col in range(6,0):
-    for row in range (6,0):
+  for col in range(0,6):
+    for row in range (0,6):
       if matrix[col][row] == 1 and matrix[col][row] == matrix[col-1][row+1] and matrix[col][row] == matrix[col-2][row+2] and matrix[col][row] == matrix[col-3][row+3]:
         print("four in a row")
 
