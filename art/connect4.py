a5fbf4cd8ac00345d5f445eed119248a9fbb7866
Andy Wells <andyandywells@me.com>
2014-08-21 11:13:17 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -2,10 +2,10 @@ global matrix
 def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
   
-  matrix[1][1] = 1
-  matrix[2][2] = 1
-  matrix[3][3] = 1
-  matrix[4][4] = 1
+  matrix[2][1] = 1
+  matrix[3][2] = 1
+  matrix[4][3] = 1
+  matrix[5][4] = 1
   
   
   
