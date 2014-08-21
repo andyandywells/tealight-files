873acbe51bb732c77f42ff2764094fbb8ef3442c
Andy Wells <andyandywells@me.com>
2014-08-21 11:28:30 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -1,1 +1,46 @@
-print "A new file!"
+global matrix
+def initialiseMatrix():
+  matrix = [[0 for i in range(7)] for j in range(7)]
+  
+ 
+  matrix[0][0] = 1
+  matrix[0][1] = 1
+  matrix[0][2] = 1
+  matrix[0][3] = 1
+  
+  print(matrix)
+  print(len(matrix))
+  return matrix
+
+
+matrix = initialiseMatrix()
+#check diagonal from left to right
+def checkDiagonalLR():
+  for row in range(0,6):
+    for col in range (0,6):
+      if matrix[row][col] == 1 and matrix[row][col] == matrix[row+1][col+1] and matrix[row][col] == matrix[row+2][col+2] and matrix[row][col] == matrix[row+3][col+3]:
+        print("four in a row")
+      
+
+#check diagonal from right to left
+def checkDiagonalRL():
+  for row in range(0,6):
+    for col in range (0,6):
+      if matrix[row][col] == 1 and matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3]:
+        print("four in a row")
+
+#check horizontal 
+def checkHorizontal():
+  for row in range(0,6):
+    for col in range (0,6):
+      if matrix[row][col] == 1 and matrix[row][col] == matrix[row+1][col] and matrix[row][col] == matrix[row+2][col] and matrix[row][col] == matrix[row+3][col]:
+        print("four in a row")
+
+#check vertical
+def checkVertical():
+  for row in range(0,6):
+    for col in range (0,6):
+      if matrix[row][col] == 1 and matrix[row][col] == matrix[row][col-1] and matrix[row][col] == matrix[row][col-2] and matrix[row][col] == matrix[row][col-3]:
+        print("four in a row")
+
+checkVertical()
