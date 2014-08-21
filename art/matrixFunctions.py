695abf17ff63e32ea6e73ae9a3b65af3cf939ff5
Andy Wells <andyandywells@me.com>
2014-08-21 12:00:47 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -55,7 +55,7 @@ def checkWin():
   checkHorizontal()
   checkDiagonalRL()
   checkDiagonalLR()
-  if checkHorizontal == True:
+  if checkHorizontal() == True:
     print("four in a row")
     
 checkWin()
