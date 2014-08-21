738730bd8e3d9d2fcf41fe8cbb56faf1b36600db
Andy Wells <andyandywells@me.com>
2014-08-21 14:22:56 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -6,9 +6,7 @@ def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
   return matrix
 
-matrix = initialiseMatrix()
-
-def checkwin():
+def checkwin(matrix):
   for row in range(0,6):
     for col in range (0,6):
       if ( matrix[row][col] == 1 and ((matrix[row][col] == matrix[row][col-1] and matrix[row][col] == matrix[row][col-2] and matrix[row][col] == matrix[row][col-3])
