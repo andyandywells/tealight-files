165cfb9ad9e78673f1f11e3b1d2b44e7841640b1
Andy Wells <andyandywells@me.com>
2014-08-22 09:11:32 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -20,6 +20,11 @@ def checkwin(matrix):
                                       or (matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3])
                                       or (matrix[row][col] == matrix[row+1][col+1] and matrix[row][col] == matrix[row+2][col+2] and matrix[row][col] == matrix[row+3][col+3]))):
         print("four in a row")
+      elif ( matrix[row][col] == -1 and ((matrix[row][col] == matrix[row][col-1] and matrix[row][col] == matrix[row][col-2] and matrix[row][col] == matrix[row][col-3])
+                                      or (matrix[row][col] == matrix[row+1][col] and matrix[row][col] == matrix[row+2][col] and matrix[row][col] == matrix[row+3][col])
+                                      or (matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3])
+                                      or (matrix[row][col] == matrix[row+1][col+1] and matrix[row][col] == matrix[row+2][col+2] and matrix[row][col] == matrix[row+3][col+3]))):
+        print("four in a row")
       
 
 matrix = initialiseMatrix()      
