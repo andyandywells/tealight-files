ff9a1a4f5c6f05e2e5304ccd4208bf4747ce6e99
Andy Wells <andyandywells@me.com>
2014-08-22 09:19:11 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -3,10 +3,10 @@ p2win = "Yellow wins"
 
 def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
-  matrix[0][5] = 1
-  matrix[1][5] = 1
-  matrix[2][5] = 1
-  matrix[3][5] = 1
+  matrix[0][6] = 1
+  matrix[1][6] = 1
+  matrix[2][6] = 1
+  matrix[3][6] = 1
   print(matrix)
   return matrix
 
