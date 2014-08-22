f78a2aaaba284ab6ca59026917df4f279d332221
Andy Wells <andyandywells@me.com>
2014-08-22 09:13:43 +0100

    Running art/matrixFunctions.py

--- art/matrixFunctions.py
+++ art/matrixFunctions.py
@@ -16,7 +16,7 @@ def checkwin():
   for row in range(0,6):
     for col in range (0,6):
       if (matrix[row][col] == 1 and ((matrix[row][col] == matrix[row][col-1] and matrix[row][col] == matrix[row][col-2] and matrix[row][col] == matrix[row][col-3])
-                                      or (matrix[row][col] == matrix[row+1][col] and matrix[row+1][col] == matrix[row+2][col] and matrix[row+2][col] == matrix[row+3][col])
+                                      or (matrix[row][col] == matrix[row+1][col] and matrix[row][col] == matrix[row+2][col] and matrix[row][col] == matrix[row+3][col])
                                       or (matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3])
                                       or (matrix[row][col] == matrix[row+1][col+1] and matrix[row][col] == matrix[row+2][col+2] and matrix[row][col] == matrix[row+3][col+3]))):
         print("four in a row")
