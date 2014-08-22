3ca6fe2db371a2a7ab355cb178b66d0acc9bfc58
Andy Wells <andyandywells@me.com>
2014-08-22 09:03:13 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -53,5 +53,5 @@ def checkWin():
   if checkHorizontal() == True or checkVertical() == True or checkDiagonalLR() == True or checkDiagonalRL() == True:
     print("four in a row")
 
-initialiseMatrix()    
+   
 checkWin()
