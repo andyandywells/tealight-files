dbb524d9d92ea358d854a079622fadfcef5d311b
Andy Wells <andyandywells@me.com>
2014-08-25 20:02:53 +0100

    Running art/connect.py

--- art/connect.py
+++ art/connect.py
@@ -1,1 +1,9 @@
-print "A new file!"
+import github.BasRegi.art.connect4_grid as grid
+from github.andyandywells.art.matrixFunctions import *
+from github.GriffithsBen.art.DetGrid import *
+
+grid.drawGrid()
+
+#BasRegi
+#andyandywells
+#NickMcAlpin
